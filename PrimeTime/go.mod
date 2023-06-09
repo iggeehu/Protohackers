df664a0540e45ef3fdd48ffb53799e73f86d37f3
matteo <hu.igor@yahoo.com>
2023-06-09 11:54:36 -0400

    new challenge

---
+++ PrimeTime/go.mod
@@ -0,0 +1,3 @@
+module iggeehu/protohackers01
+
+go 1.20
---
+++ PrimeTime/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io"
+
+	"log"
+
+	"net"
+)
+
+func main() {
+
+	PORT := ":10000"
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+
+	fmt.Println("Server is listening.")
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	fmt.Printf("Serving %s\n", conn.RemoteAddr().String())
+	packet := make([]byte, 0)
+	tmp := make([]byte, 1)
+	defer conn.Close()
+	for {
+		_, err := conn.Read(tmp)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("read error:", err)
+			}
+			println("END OF FILE")
+			break
+		}
+		packet = append(packet, tmp...)
+	}
+	conn.Write(packet)
+	fmt.Printf("message is %s", string(packet))
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true}
