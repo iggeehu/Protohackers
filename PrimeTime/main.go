e5f7f74379a503804f06fca6f807c777baa250ae
matteo <hu.igor@yahoo.com>
2023-06-13 16:04:05 -0400

    fly-toml, attempting a loop within handle for 'wait for another request'

--- PrimeTime/main.go
+++ PrimeTime/main.go
@@ -31,51 +31,55 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	fmt.Printf("Serving %s\n", conn.RemoteAddr().String())
-	packet := make([]byte, 0)
-	tmp:=make([]byte, 1024)
-	defer conn.Close()
-	
-	for{
-		n, err := conn.Read(tmp)
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("read error:", err)
+		
+		packet := make([]byte, 0)
+		tmp:=make([]byte, 1024)
+		for{
+			n, err := conn.Read(tmp)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Println("read error:", err)
+				}
+				break
 			}
-			println("END OF FILE")
-			break
+			packet = append(packet, tmp[:n]...)
 		}
-		packet = append(packet, tmp[:n]...)
-	}
+		if(len(packet)==0){ continue }
 
-	if !json.Valid(packet) {
-		conn.Write([]byte("Malformed JSON"))	
-		fmt.Println("packet is not JSON")
-		conn.Close()
-	} else{
-		fmt.Println("packet is JSON")
-		//decode JSON object in packet
-		obj:=make(map[string]interface{})
-		err:=json.Unmarshal(packet, &obj)
-		methodVal, methodOk:=obj["Method"]
-		numberVal, numberOk:=obj["Prime"]
+		fmt.Printf("message is %s", string(packet))
 
-		if err!=nil{
-		  fmt.Println(err)
-		}
-
-
-		if !methodOk || !numberOk || methodVal!="isPrime"||
-		reflect.TypeOf(numberVal).String()!= "float64" {
-			conn.Write([]byte("Malformed JSON"))
+		if !json.Valid(packet) {
+			conn.Write([]byte("Malformed JSON"))	
+			fmt.Println("packet is not JSON")
 			conn.Close()
-		}else{
-		response:=make(map[string]interface{})
-		response["Prime"]=isPrime(int(numberVal.(float64)))
-		response["Method"]="isPrime"
-		jsonResponse, _:=json.Marshal(response)
-		conn.Write(jsonResponse)
-		}
-		}	
+			return
+		} else{
+			fmt.Println("packet is JSON")
+			//decode JSON object in packet
+			obj:=make(map[string]interface{})
+			err:=json.Unmarshal(packet, &obj)
+			methodVal, methodOk:=obj["Method"]
+			numberVal, numberOk:=obj["Prime"]
+
+			if err!=nil{
+				fmt.Println(err)
+			}
+
+			if !methodOk || !numberOk || methodVal!="isPrime"||
+			reflect.TypeOf(numberVal).String()!= "float64" {
+				conn.Write([]byte("Malformed JSON"))
+				conn.Close()
+				return
+			}else{
+			response:=make(map[string]interface{})
+			response["Prime"]=isPrime(int(numberVal.(float64)))
+			response["Method"]="isPrime"
+			jsonResponse, _:=json.Marshal(response)
+			conn.Write(jsonResponse)
+			}
+			}	
+		
+		
 	}
 
 func isPrime(num int) bool {
