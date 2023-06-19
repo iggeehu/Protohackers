c4235589948590bc8fdf99e910f374c64b822f37
matteo <hu.igor@yahoo.com>
2023-06-19 12:19:17 -0400

    attempt to fix empty message EOF

--- PrimeTime/main.go
+++ PrimeTime/main.go
@@ -30,58 +30,40 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
+	messageCount:=0
 	fmt.Printf("Serving %s\n", conn.RemoteAddr().String())
 	defer conn.Close()
+	//each iteration is a new request
 	for{
 		packet := make([]byte, 0)
 		tmp:=make([]byte, 1024)
 		for{
 			n, err := conn.Read(tmp)
+			//if there is an error
 			if err != nil {
-				if err != io.EOF {
-					fmt.Println("read error:", err)
+				//if EOF, handle packet
+				if err == io.EOF {
+					if(len(packet)!=0){
+						messageCount++
+						fmt.Println("END OF FILE. Message is",  string(packet))
+						fmt.Println("Message count is", messageCount)
+						connstat:=respondToPacket(packet, conn)
+						if(connstat==1){
+							fmt.Println("Connection closed")
+							return
+							}
+					}
+				//if other error
+			}else{
+				fmt.Println("read error:", err)
 				}
 				break
 			}
 			packet = append(packet, tmp[:n]...)
 		}
-
-
-		fmt.Printf("message is %s", string(packet))
-
-		if !json.Valid(packet) {
-			conn.Write([]byte("Malformed JSON"))	
-			fmt.Println("packet is not JSON")
-			conn.Close()
-			return
-		} else{
-			fmt.Println("packet is JSON")
-			//decode JSON object in packet
-			obj:=make(map[string]interface{})
-			err:=json.Unmarshal(packet, &obj)
-			methodVal, methodOk:=obj["Method"]
-			numberVal, numberOk:=obj["Prime"]
-			fmt.Println(obj)
-			if err!=nil{
-				fmt.Println(err)
-			}
-
-			if !methodOk || !numberOk || methodVal!="isPrime"||
-			reflect.TypeOf(numberVal).String()!= "float64" {
-				conn.Write([]byte("Malformed JSON"))
-				conn.Close()
-				return
-			}else{
-			response:=make(map[string]interface{})
-			response["Prime"]=isPrime(int(numberVal.(float64)))
-			response["Method"]="isPrime"
-			jsonResponse, _:=json.Marshal(response)
-			fmt.Println(string(jsonResponse))
-			conn.Write(jsonResponse)
-			}
-		}	
 	}
 }
+
 
 func isPrime(num int) bool {
 	for i := 2; i < num; i++ {
@@ -91,3 +73,38 @@ func isPrime(num int) bool {
 	}
 	return true}
 
+//0 means conn is not closed at the end, 1 means conn is closed
+func respondToPacket(packet []byte, conn net.Conn) int {
+	if !json.Valid(packet) {
+		conn.Write([]byte("Malformed JSON"))	
+		fmt.Println("packet is not JSON")
+		conn.Close()
+		return 1
+	} else{
+		fmt.Println("packet is JSON")
+		//decode JSON object in packet
+		obj:=make(map[string]interface{})
+		err:=json.Unmarshal(packet, &obj)
+		if(err!=nil){
+			fmt.Println("unmarshal error: ", err)
+		}
+		methodVal, methodOk:=obj["method"]
+		numberVal, numberOk:=obj["number"]
+		fmt.Println("numberVal is", reflect.TypeOf(numberVal).Kind())
+
+		if !methodOk || !numberOk || methodVal!="isPrime"||
+		reflect.TypeOf(numberVal).Kind()!=reflect.Float64{
+			
+			fmt.Println("Packet is JSON but Malformed")
+			conn.Write([]byte("Malformed JSON"))
+			conn.Close()
+			return 1
+		}else{
+		response:=make(map[string]interface{})
+		response["Prime"]=isPrime(int(numberVal.(float64)))
+		response["Method"]="isPrime"
+		jsonResponse, _:=json.Marshal(response)
+		conn.Write(jsonResponse)
+		return 0
+		}
+	}}
