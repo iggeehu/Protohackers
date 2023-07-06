974b4cbdf1d6644a4675ef932c00eb6978562468
matteo <hu.igor@yahoo.com>
2023-07-06 15:34:26 -0400

    complete

--- MeansToAnEnd/main.go
+++ MeansToAnEnd/main.go
@@ -1,15 +1,29 @@
 package main
+
 //Each client tracks a different asset
 //Each connection is a separate session, each session a different asset
 
-
 import (
-"net", 
-"fmt",
-"encoding/hex",
-"encoding/binary",
-
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sort"
 )
+
+type Price struct{
+	Time uint32
+	Amount uint32
+}
+
+type ByTime []Price
+
+func (p ByTime) Len() int           { return len(p) }
+func (p ByTime) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByTime) Less(i, j int) bool { return p[i].Time < p[j].Time }
+
 
 //main.go
 func main(){
@@ -26,16 +40,18 @@ func main(){
 			fmt.Println(err)
 			continue
 		}
+		fmt.Println("Listening...")
 		go handle(c)
 	}
 }
 
 func handle(conn net.Conn){
-	prices:=make(map[int32]int32)
+	fmt.Printf("Serving %s\n", conn.RemoteAddr().String())
+	record:=make([]Price, 0)
 	defer conn.Close()
 	packet:=make([]byte, 9)
 	for{
-		bytesRead, err:=io.ReadFull(conn, packet)
+		_, err:=io.ReadFull(conn, packet)
 		if err!=nil{
 			if err==io.EOF{
 				fmt.Println("EOF")
@@ -43,64 +59,55 @@ func handle(conn net.Conn){
 			println("error reading from connection")
 			break
 		}
-		type, first, second, err:=parse(packet)
-		switch type{
+		ptype, first, second, err:=parse(packet)
+		switch ptype{
 		case "I":
-				prices[first]=insertPrice(second)
+				curr:=Price{first, second}
+				record=append(record, curr)
 		case "Q":
-				keys=rangeOfKeys(first, second, prices)
-				sum:=0
-				for _, k := range keys{
-					sum+=prices[k]
+				deliverable := make([]byte, 4)
+
+				sort.Sort(ByTime(record))
+				bi := sort.Search(len(record), func(i int) bool { return record[i].Amount >= first })
+				ei:= sort.Search(len(record), func(i int) bool { return record[i].Amount > second })
+				//If there are no samples within the requested period, or if mintime comes after maxtime, the value returned must be 0.
+				if bi==len(record) || ei==len(record){
+					binary.LittleEndian.PutUint32(deliverable, 0)
+					conn.Write(deliverable)
+					break
 				}
-				conn.Write(sum/len(keys))
-
+				var sum uint32=0
+				for i:=bi; i<=ei-1; i++{
+						sum+=record[i].Amount
+				}
+				avg:=sum/(uint32)(ei-bi)
+				binary.LittleEndian.PutUint32(deliverable, avg)
+				_, err = conn.Write(deliverable)
+		
 			}
-
+	}
 }
 
-func rangeOfKeys(start int32, end int32, prices map[int32]int32) int32[]{
-	keys := make([]int, len(prices))
-	i := 0
-	for k := range prices {
-		keys[i] = k
-		i++
-	}
-	sort.Ints(keys)
-	for i, k := range keys {
-		if(k>=first){
-			key=keys[i:]
-		}
-		if(k>second){
-			key=keys[:i]
-		}
-	}
-	return keys
-}
 
-func parse(msg []bytes) (type byte, first int32, second int 32, err error){
+
+func parse(msg []byte) (ptype string, first uint32, second uint32, err error){
 	if len(msg)!=9{
-		return 0,0,0,errors.New("invalid message length")
+		return "error",0,0,errors.New("invalid message length")
 	}
 	fmt.Println("message is ", msg[0]) 
 	switch msg[0] {
 	case 0x71:
-		type="Q"
+		ptype="Q"
 	case 0x49:
-		type="I"
+		ptype="I"
 	default:
-		return 0,0,0,errors.New("invalid message type")
-
+		return "",0,0,errors.New("invalid message type")
+	}
 	first=binary.BigEndian.Uint32(msg[1:5])
 	second=binary.BigEndian.Uint32(msg[5:9])
-	return type, first, second, nil	
-	}
+	fmt.Println("first is ", first, "second is ", second, "type is ", ptype)
+	return ptype, first, second, nil	
+	
 }
 
 
-
-
-//handleConnection
-
-
-//writeInsert
